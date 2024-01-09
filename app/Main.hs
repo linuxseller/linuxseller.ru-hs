828098ab332186fb4d7480291d95253d04555c56
linuxseller <shibanutyitokom@yandex.ru>
2024-01-09 07:15:37 +0500

    login page added

--- app/Main.hs
+++ app/Main.hs
@@ -2,25 +2,39 @@
 {-# LANGUAGE QuasiQuotes           #-}
 {-# LANGUAGE TemplateHaskell       #-}
 {-# LANGUAGE TypeFamilies          #-}
-
+{-# LANGUAGE MultiParamTypeClasses #-}
 import Yesod
 import Text.Hamlet (HtmlUrl, hamlet)
 import Text.Blaze.Html.Renderer.String (renderHtml)
-import Data.Text (Text, pack)
+import Data.Text (Text, pack, unpack)
 import Database.SQLite.Simple
 import Database.SQLite.Simple.FromRow
+import qualified Web.ClientSession   as CS
 
 import System.IO.Unsafe
 import GHC.IO (unsafePerformIO)
 import Data.List
+import Data.Maybe
+import qualified Data.Map as Map
 
 import Import
 {-----------------------
 import DB (getStory, getStroriesList, Story, StoryId)
 import Handlers
 -----------------------}
-type StoryId = Int
+
+type UserId = Int
 type Login = String
+type Password = String
+type Email = String
+type CreationDate = String
+data User = User UserId Login Password Email CreationDate
+defaultUser = User (-1) "" "" "" ""
+type Users = [User]
+instance FromRow User where
+    fromRow = User <$> field <*> field <*> field <*> field <*> field
+
+type StoryId = Int
 type StoryName = String
 type StoryText = String
 type StoryDateStamp = String
@@ -29,43 +43,79 @@ type Stories = [Story]
 instance FromRow Story where
     fromRow = Story <$> field <*> field <*> field <*> field <*> field
 
-getStroriesList :: IO Stories
-getStroriesList = do
-    conn <- open "test.db"
+db :: FilePath
+db = "test.db"
+
+getUsersList :: IO Users
+getUsersList = do
+    conn <- open db
+    r <- query_ conn (Query $ pack "SELECT * FROM users") :: IO Users
+    close conn
+    return r
+
+getStoriesList :: IO Stories
+getStoriesList = do
+    conn <- open db
     r <- query_ conn (Query $ pack "SELECT * FROM stories") :: IO Stories
     close conn
     return r
 
 getStory id = do
-    conn <- open "test.db"
+    conn <- open db
     r <- query_ conn ( Query $ pack ("SELECT * FROM stories WHERE StoryId=="<>show id)) :: IO Stories
     close conn
     return $ Just $ head r
 
-data MyRoute = Home
+getUser login = do
+    conn <- open db
+    r <- query_ conn ( Query $ pack ("SELECT * FROM users WHERE login='"<>login<>"'")) :: IO Users
+    close conn
+    return $ Just $ head r
 
-render :: MyRoute -> [(Text, Text)] -> Text
-render Home _ = "/home"
-
+getUserPassword :: User -> Password
+getUserPassword (User _ _ p _ _) = p
 
 data HelloWorld = HelloWorld
 
 mkYesod "HelloWorld" [parseRoutes|
-/                 HomeR             GET
-/stories          StoriesR          GET
+/                 HomeShowR         GET
+/stories          StoriesShowR      GET
 /stories/#StoryId StoriesShowStoryR GET
+/login            LoginShowR        GET POST
 |]
 
-instance Yesod HelloWorld
-getHomeR :: Handler Html
-getHomeR = getStoriesR
 
-{-# NOINLINE getStoriesR #-}
-getStoriesR :: Handler Html
-getStoriesR = defaultLayout $ do
+instance Yesod HelloWorld
+
+instance RenderMessage HelloWorld FormMessage where
+    renderMessage _ _ = defaultFormMessage
+
+{-# NOINLINE getLoginShowR #-}
+getLoginShowR :: Handler Html
+getLoginShowR = defaultLayout $ do
+    setTitle "Login"
+    $(widgetFile "login")
+
+{-# NOINLINE postLoginShowR #-}
+postLoginShowR :: Handler ()
+postLoginShowR = do
+    (login, passwordd) <- runInputPost $ (,) <$> ireq textField "login" <*> iopt textField "password"
+    let dbpassword = getUserPassword $ fromMaybe defaultUser $ unsafePerformIO $ getUser $ unpack login
+    case passwordd of
+        Nothing -> redirect LoginShowR
+        Just password -> if unpack password==dbpassword then setSession "login" login else redirect LoginShowR
+    redirect HomeShowR
+
+getHomeShowR :: Handler Html
+getHomeShowR = getStoriesShowR
+
+{-# NOINLINE getStoriesShowR #-}
+getStoriesShowR :: Handler Html
+getStoriesShowR = defaultLayout $ do
+    sess <- getSession
     setTitle "Stories Index"
     $(widgetFile "stories")
-    where stories = unsafePerformIO getStroriesList
+    where stories = unsafePerformIO getStoriesList
 
 getStoriesShowStoryR :: Int -> Handler Html
 getStoriesShowStoryR id = defaultLayout $ do
